870cd36adc362e1a6458f0a9c9100c9cf9ad5c2f
nina <nina.balachandran@sungardas.com>
2017-01-26 00:21:28 -0800

    basic routing

--- views/components/App.js
+++ views/components/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Link,Router, Route, IndexRedirect ,hashHistory} from 'react-router';
-import Friends from  './friends.js';
-import Groups from  './groups.js';
+import { Link, Router, Route, IndexRedirect ,hashHistory} from 'react-router';
+import Friends from  './friends';
+import Groups from  './groups';
 
 class defaultPage extends React.Component{
     render(){
@@ -12,10 +12,39 @@ class defaultPage extends React.Component{
     }
 }
 
-class App extends React.Component{
+class inProgress extends React.Component{
     render(){
-        return <div>App</div>
+        return <div>
+             Not yet implemented
+        </div>
     }
 }
 
-ReactDOM.render(<App/>, document.getElementById('content'));
+class App extends React.Component{
+    render(){
+        return <div>
+            <ul className = "header">
+                <li><Link to="/friends" activeClassName="active"> Friends </Link></li>
+                <li><Link to="/groups" activeClassName="active"> Groups </Link></li>
+                <li><Link to="/broadcasts" activeClassName="active"> Broadcasts</Link></li>
+            </ul>
+            <div className = "content">
+                {this.props.children}
+            </div>
+        </div>
+    }
+}
+
+ReactDOM.render(<Router history = {hashHistory}>
+    <Router path="/" component={App}>
+        <IndexRedirect to="/friends"/>
+        <Route path="/friends" components={Friends}>
+        </Route>
+        <Route path="/groups" components={Groups}>
+        </Route>
+        <Route path="/broadcasts" components={inProgress}>
+        </Route>
+        <Route path="*" component ={defaultPage}>
+        </Route>
+    </Router>
+</Router>, document.getElementById('content'));
